b0e33010a39470755f5b350c60b85253827525f3
Rubén Álvarez González <rubenag83@gmail.com>
2021-01-23 10:12:59 +0100

    Update TeamPicker.js
    Se incorpora apariencia agradable con Reactrap a la página de inicio

--- src/components/TeamPicker.js
+++ src/components/TeamPicker.js
@@ -1,29 +1,49 @@
 import React from 'react';
-import { getFunName } from '../helpers';
+import { Jumbotron, Button, Container, Row, Col, Input, InputGroup, InputGroupAddon } from 'reactstrap';
 
 class TeamPicker extends React.Component {
     
-    teamName = React.createRef();
+    teamNameRef = React.createRef();
 
 
 
     goToTeam = (event) => {
         event.preventDefault();
-        this.props.history.push(`/team/${this.teamName.current.value}`)
+        console.log(this.teamNameRef.current.value);
+        //this.props.history.push(`/team/${this.teamNameRef.current.value}`)
     }
 
     render(){
         return (
-            <form className="team-selector" onSubmit={this.goToTeam}>
-                <h2>Please Enter a Team Name</h2>
-                <input 
-                    type="text" 
-                    ref={this.teamName}
-                    required 
-                    placeholder="Team Name"
-                />
-                <button type="submit">visit Team</button>
-            </form>
+            <Container>
+                <Row>
+                    <Col>
+                        <Jumbotron>
+                            <h1 className="display-3">Welcome to Quick Team Notes!</h1>
+                            <p className="lead">This is a ligth documentation app where your team will write brief summary talk.</p>
+                            <hr className="my-2" />
+                            <p>You have to write bellow the team name whom you want to communicate.</p>
+                            <p className="lead">
+                            <form onSubmit={this.goToTeam}>
+                                <InputGroup>
+                                    <Input 
+                                        type="text" 
+                                        ref={this.teamNameRef}
+                                        required 
+                                        placeholder="Team Name"
+                                    />
+                                    <InputGroupAddon addonType="append">
+                                        <Button type="submit" color="success">Visit Team</Button>
+                                    </InputGroupAddon>
+                                    
+                                </InputGroup>
+                            </form>
+                        
+                            </p>
+                        </Jumbotron>
+                    </Col>
+                </Row>
+            </Container>
         )
     }
 }
